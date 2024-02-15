2a0c065c401d65af344b5de453f40cc17e35dd68
Noodledoodlemacaroodle <160121077+Noodledoodlemacaroodle@users.noreply.github.com>
2024-02-15 22:37:51 +0100

    Add files via upload

---
+++ 3D Gravity Test/Camera3D.gd
@@ -0,0 +1,13 @@
+extends Camera3D
+@onready var camera_root = $"../Camera_root"
+var speed
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
